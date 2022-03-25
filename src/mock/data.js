8a682d9e7f01bda02a20f70ba9fd6bb65dc1cc18
HammedTiamiyu <hammedtiamity18@gmail.com>
2022-03-25 01:47:03 +0100

    some project corrections made

--- src/mock/data.js
+++ src/mock/data.js
@@ -34,7 +34,7 @@ export const projectsData = [
     img: 'pagination_screenshot.png',
     title: 'Github Users Front-end Pagination',
     info: 'In this project, with datas coming from github API some github users were rendered with front-end pagination',
-    info2: '',
+    info2: 'The project was built with the knowledge of API data fetch, useEffect,useState, react Router, some Math logics',
     url: 'https://react-pagination-display.netlify.app/',
     repo: 'https://github.com/hammedTiamiyu/react-pagination', // if no repo, the button will not show up
   },
@@ -42,9 +42,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'cocktails_menu.png',
     title: 'Cocktails',
-    info: 'In this project, a third party API was used to fetch different cocktails datas. At initial loading of the app, just some few details are rendered - like display image, name, glass description - along with a  button ( for details) ',
-    info2:
-      'The detail button recalls the api with a unique id param and fetch some more details about a particular cocktail e.g the recipes and method of preparation. The project also have filter function to filter coktail list',
+    info: 'In this (react-app) project, a third party API was used to fetch different cocktails datas. At initial loading of the app, just some few details are rendered - like display image, name, glass description - along with a  button ( for details). Then the detail button button route the app to the detail page and then fetch a particular drinks details such as the recipes and method of preparation ',
+    info2: 'The project was built with the knowledge of API data fetch, useEffect,useState, react Router ',
     url: 'https://react-cocktails-menu.netlify.app/',
     repo: 'https://github.com/hammedTiamiyu/react-projects-cocktails-menu', // if no repo, the button will not show up
   },
@@ -52,7 +51,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'amazhon-clone.png',
     title: 'Fake-Amazon-Clone',
-    info: 'In this project, a copy amazon website was built using react context API, reducer etc. And this web app includes many great features  like user authentication ( using google firebase ), carting, payment etc',
+    info: 'In this (react-app) project, a copy amazon website was built using tech react context API, reducer etc. And this web app includes many great features  like user authentication ( using google firebase ), carting, payment etc',
     info2: '',
     url: 'https://amazhon-clone-v1.web.app/',
     repo: 'https://github.com/hammedTiamiyu/amazon-clone-v1', // if no repo, the button will not show up
@@ -61,7 +60,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'react-menu.png',
     title: 'Menu',
-    info: 'Another small yet beautiful react project that i practised. Many food items were displayed with some catchy image. There are few categories buttons which are dynamically added to filter the menu',
+    info: 'Another small yet beautiful react project that i practised. Many food items were displayed with some catchy image. There are few categories buttons which are dynamically added to filter the menu.',
     info2: '',
     url: 'https://react-menu-display.netlify.app/',
     repo: 'https://github.com/hammedTiamiyu/react-menu', // if no repo, the button will not show up
@@ -70,18 +69,27 @@ export const projectsData = [
     id: nanoid(),
     img: 'grocery-bud.png',
     title: 'Grocey Bud',
-    info: 'This is a simple grocery bud app built with react. It has i/o logic that allows one to create a list of some grocery items (stored on the browser local storage) which can then be editted or deleted  ',
+    info: 'This is a simple grocery bud app built with react. It has i/o logic that allows one to create a list of some grocery items (stored in the browser local storage) which can then be editted or deleted. A simple project with CRUDE functionalities. ',
     info2: '',
     url: 'https://react-grocerybuds.netlify.app/',
     repo: 'https://github.com/hammedTiamiyu/react-grocerybud', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'react-moviedb.png',
+    title: 'Movie DB',
+    info: 'Another exciting react project built the knowldege of data fetching with third party API, useEffect, react Context API, react Router, query string paramas ',
+    info2: '',
+    url: 'https://react-movie-database1.netlify.app/',
+    repo: 'https://github.com/hammedTiamiyu/react-movieDB', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: '2347065072820',
+  btn: 'Email',
+  email: 'hammedtiamiyu18@gmail.com',
 };
 
 // FOOTER DATA
